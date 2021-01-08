a9c425cfcd9e2f1dc899bad20f678e2d9dfb2e5e
Katharina Schüßler <schuessler.katharina@gmail.com>
2021-01-08 11:33:40 +0100

    Add ETL steps to pipeline in script

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,68 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    INPUT:
+        messages_filepath - path to the csv file holding messages data
+        categories_filepath - path to the csv file holding categories data
+    OUTPUT:
+        df - a dataframe including all columns from the messages and categories files
+    """
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+    # merge datasets
+    df = messages.merge(categories, how='outer', on=['id'])
+
+    return df
 
 
 def clean_data(df):
-    pass
+    """
+    INPUT:
+        df - a dataframe including all columns from the messages and categories files
+    OUTPUT:
+        df - a dataframe with separate columns for all response categories without duplicates
+    """
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand=True)
+    # extract new column names using the first row of the categories dataframe
+    row = categories[:1].values.flatten().tolist()
+    category_colnames = [x[:-2] for x in row]
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = [x.strip()[-1] for x in categories[column]]
+
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    # drop the original categories column from `df`
+    df = df.drop(columns=['categories'])
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+
+    # drop duplicates
+    df = df[-df.duplicated()]
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    INPUT:
+        df - cleaned dataframe
+    OUTPUT:
+        ...
+    """
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql('messages', engine, index=False)
 
 
 def main():
